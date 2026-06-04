8032b53c6c2b9b02eb0efd31ff1879f62501ad36
Syed Sufyan <syedsufyanulhassan910-afk>
2026-06-04 16:10:53 +0500

    first commit

---
+++ 1.js
@@ -0,0 +1,33 @@
+console.log( "my console")
+document.writeln("my document")
+
+// let no=1
+// do{
+//     console.log("my work is don")
+//     no++
+// }while(no<=10)
+
+// let data=[1,2,3,4,5,6]
+// for(let Data of data){
+//     console.log(Data)
+// }
+
+// let data =["ali","sufyan","haroon"]
+// for(let Data in data){
+//     console.log(Data[Data])
+// }
+
+// console.log(data.length)
+// console.log(data[4])
+
+// let str="ali   "
+// let str2="HAROON"
+// console.log(str.concat(str2))
+
+// console.log(str.trim().concat(str2))
+
+// console.log(str.toUpperCase())
+// console.log(str2.toLowerCase())
+
+// console.log2(str.slice(2,6))
+
