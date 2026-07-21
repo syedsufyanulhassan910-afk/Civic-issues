c4cf4edc8ca7da2da60102ae6cdba4b27548e9cc
Syed Sufyan <syedsufyanulhassan910@gmail.com>
2026-07-21 16:47:02 +0500

    Add files via upload

---
+++ modules/modules/app.js
@@ -0,0 +1,9 @@
+import { add, division, multiply, substraction } from './mathUtils.js';
+
+import greet from './mathUtils.js';
+
+console.log(add(12, 8));        
+console.log(substraction(12, 8));  
+console.log(multiply(10, 10));
+console.log(division(1000, 2));
+console.log(greet("Wahab")); 
