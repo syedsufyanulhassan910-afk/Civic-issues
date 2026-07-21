1227c90cb3dc18a2abaec1635753ca94a76b746a
Syed Sufyan <syedsufyanulhassan910@gmail.com>
2026-07-21 16:48:17 +0500

    Delete modules directory

--- modules/app.js
+++
@@ -1,9 +0,0 @@
-import { add, division, multiply, substraction } from './mathUtils.js';
-
-import greet from './mathUtils.js';
-
-console.log(add(12, 8));        
-console.log(substraction(12, 8));  
-console.log(multiply(10, 10));
-console.log(division(1000, 2));
-console.log(greet("Wahab")); 
